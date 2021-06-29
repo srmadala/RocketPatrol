ab15f255ab370ed51229bfd60ce61e274c3c74a0
Harsha <srmadala@ucsc.edu>
2021-06-28 22:43:27 -0700

    issues with spelling again. Fixed

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -51,7 +51,7 @@ class Play extends Phaser.Scene {
         }
     }
 
-    checkCollisions(Rocket, ship) {
+    checkCollision(rocket, ship) {
         // simple AABB checking
         if (rocket.x < ship.x + ship.width && rocket.x + rocket.width > ship.x && rocket.y < ship.y + ship.height && rocket.height + rocket.y > ship. y) {
             return true;
