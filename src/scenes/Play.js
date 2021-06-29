929065a04d1c309891521be999cbf2ef26a71a90
Harsha <srmadala@ucsc.edu>
2021-06-28 20:43:51 -0700

    have starfield background working

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,7 +3,16 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
+    preload() {
+        // load images/title sprites
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
     create() {
+        // place tile sprites
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // white borders
@@ -12,7 +21,8 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
     }
-    //init() {
-    //    
-    //}
+
+    update() {
+        this.starfield.tilePositionX -= 4;
+    }
 }
